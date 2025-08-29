32cd99205628e76cc8a5ad8e8f9be9bdc9ab1903
Dmitry Kalinin <kalinin.mitko@gmail.com>
2025-08-29 13:38:49 +0300

    1.0.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="econt",
-    version="1.0.3",
+    version="1.0.4",
     author="Dmitry Kalinin",
     packages=["econt"],
     url="https://github.com/lesthetique-store/econt",
